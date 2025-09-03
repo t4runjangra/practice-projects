3694e5b15b621707dcfeb6afde31ecbcdf57e7c6
tarun <tarunjangra1903@gmail.com>
2025-09-03 12:08:36 +0530

    completed the expense tracker

--- expense-Tracker/script.js
+++ expense-Tracker/script.js
@@ -2,18 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const expenseForm = document.getElementById("expense-form")
     const expenseName = document.getElementById("expense-name")
     const expenseAmount = document.getElementById("expense-amount")
-    const submitButton = document.querySelector("#submit")
     const expenseList = document.getElementById("expense-list")
-    const total = document.getElementById("total")
     const totalAmount = document.getElementById("total-amount")
-    
-    let expense = JSON.parse(localStorage.getItem("Expense"))||[];
+
+    let expense = JSON.parse(localStorage.getItem("Expense")) || [];
+    let totalExpense = calculateTotal();
+    updateTotal();
+    renderExpense();
 
     expenseForm.addEventListener("submit", (e) => {
         e.preventDefault();
         let name = expenseName.value.trim();
-        let amount = parseInt(expenseAmount.value.trim());
-        if(name!= "" && !isNaN(amount) && amount>0){
+        let amount = parseFloat(expenseAmount.value.trim());
+        if (name != "" && !isNaN(amount) && amount > 0.00) {
             let newExpense = {
                 id: Date.now(),
                 name, // this is a new method in which you can define the key value by writing the key only it automatically understand that that key and value both are going to be same
@@ -21,23 +22,45 @@ document.addEventListener("DOMContentLoaded", () => {
             }
             expense.push(newExpense);
             saveExpense();
-            renderExpense(name, amount)
+            updateTotal()
+            renderExpense()
             expenseAmount.value = ""
             expenseName.value = ""
         }
     })
-    
-    function renderExpense(name ,amount){
-        const li = document.createElement("li");
-        li.classList.add("expense-item");
-        li.innerHTML = `
-            <span>${name} - $${amount}</span>
-            <button class="delete-btn">Delete</button>
-        `
-        
+
+    function renderExpense() {
+        expenseList.innerHTML = ""
+        expense.forEach((expenses, index) => {
+            const li = document.createElement("li");
+            li.classList.add("expense-item");
+            li.innerHTML = `
+                <span>${expenses.name} - $${expenses.amount}</span>
+                <button data-id="${expenses.id}" class="delete-btn">Delete</button>
+            `
+            expenseList.appendChild(li)
+
+            li.querySelector("button").addEventListener("click", (e) => {
+                e.stopPropagation()
+                expense = expense.filter(exp => exp.id !== expenses.id)
+                li.remove()
+
+                updateTotal()
+                saveExpense()
+
+            })
+        })
+    }
+    function calculateTotal() {
+        return expense.reduce((sum, expense) => (sum + expense.amount), 0)
+    }
+    function updateTotal() {
+        totalExpense = calculateTotal()
+        totalAmount.textContent = totalExpense.toFixed(2);
     }
 
-    function saveExpense(){
+
+    function saveExpense() {
         localStorage.setItem("Expense", JSON.stringify(expense));
     }
 })
